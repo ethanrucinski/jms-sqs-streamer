9f9cf2de77284ca2707a8702628b00e219e7b41e
Ethan Rucinski <ethan.rucinski@gmail.com>
2020-07-17 15:01:26 -0500

    adding SQS publishing logic

--- src/main/java/com/ethanrucinski/jmssqsstreamer/jms/jmsListener.java
+++ src/main/java/com/ethanrucinski/jmssqsstreamer/jms/jmsListener.java
@@ -8,17 +8,34 @@ import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.AmazonSQSClientBuilder;
+import com.amazonaws.services.sqs.model.AmazonSQSException;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
+
 @EnableJms
 @Component
 public class jmsListener {
 
     // Class responsible for listening to first queue and receiving each message
+    AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
+
     @JmsListener(destination = "${jmslistener.queueName}", containerFactory = "jmsListenerContainerFactory")
     public void handle(final Message message) {
         if (message instanceof TextMessage) {
             final TextMessage tm = (TextMessage) message;
             try {
-                System.out.println(tm.getText());
+                //System.out.println(tm.getText());
+
+                try {
+                    SendMessageRequest send_msg_request = new SendMessageRequest().withQueueUrl("${sqs.sqsQueueURL}")
+                        .withMessageBody(tm.getText()).withDelaySeconds(0);
+                        
+                sqs.sendMessage(send_msg_request);        
+                } catch (AmazonSQSException e) {
+                    e.printStackTrace();
+                }
+                
             } catch (final JMSException e) {
                 e.printStackTrace();
             }
